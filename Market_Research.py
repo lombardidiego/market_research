1a50e660bd644cc0964ac8ae74456bbd2c99468e
lombardidiego <diego.lombardi.boscan@gmail.com>
2019-10-11 12:34:19 -0500

    This is the First Part of the Final Project

--- Market_Research.py
+++ Market_Research.py
@@ -5,7 +5,7 @@
 # 
 # Express Market Research, with this code you can make a quick investigation of any location, including aspects such as number of premises, distances, and even a previous analysis of names.
 
-# In[1]:
+# In[60]:
 
 
 import numpy as np
@@ -33,10 +33,23 @@ import plotly.express as px
 from plotly.subplots import make_subplots
 
 from sklearn.cluster import KMeans
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn import manifold, datasets
+from sklearn.datasets.samples_generator import make_blobs
 
+import scipy
+from scipy import ndimage 
+from scipy.cluster import hierarchy 
+from scipy.spatial import distance_matrix 
+ 
 import folium
+from folium import IFrame
 
 from wordcloud import WordCloud, STOPWORDS
+import re
 
 print('Libraries imported.')
 
@@ -48,8 +61,8 @@ print('Libraries imported.')
 # In[2]:
 
 
-address = 'Hamilton, IN'
-radius = 2500
+address = '101 9 Ave SW, Calgary, AB T2P 1J9, Canada'
+radius = 700
 
 geolocator = Nominatim(user_agent="delivery_zones")
 location = geolocator.geocode(address)
@@ -241,7 +254,7 @@ print ('The average distancie from {} is {}.'.format(address, nearby_attribute['
 # In[10]:
 
 
-nearby_attribute['Distance'].plot(kind='hist', figsize=(12, 6))
+nearby_attribute['Distance'].plot(kind='hist', figsize=(12, 6), color=(0.2, 0.4, 0.6, 0.6))
 
 plt.title('Histogram of the distance of venues in a radius of {} meters.'.format(radius)) # add a title to the histogram
 plt.ylabel('Number of venues') # add y-label
@@ -286,6 +299,202 @@ plt.imshow(wordcloud, interpolation = 'bilinear')
 plt.axis('off')
 plt.tight_layout(pad=0)
 plt.show()
+
+
+# In[13]:
+
+
+nearby_venues.head()
+
+
+# ### Clustering
+
+# In[14]:
+
+
+venues_group = nearby_venues[['Venue Category', 'Venue']].groupby(by='Venue Category').count().reset_index()
+venues_group.shape
+
+
+# #### Reduce the number of categories
+
+# In[15]:
+
+
+venues_group['Venue Category'].unique()
+
+class_restaurant = ['Restaurant', 'Steakhouse', 'American Restaurant', 'Asian Restaurant', 'Mediterranean Restaurant',
+                   'Brazilian Restaurant', 'Chinese Restaurant', 'Diner', 'Eastern European Restaurant', 
+                    'Vegetarian / Vegan Restaurant', 'Mexican Restaurant', 'Middle Eastern Restaurant', 
+                    'Eastern European Restaurant', 'French Restaurant', 'Italian Restaurant', 'Japanese Restaurant', 
+                    'Mediterranean Restaurant', 'New American Restaurant', 'Seafood Restaurant', 'Burger Joint']
+class_coffe = ['Breakfast Spot', 'Café', 'Deli / Bodega', 'Coffee Shop', 'Bakery']
+class_bar = ['Pub', 'Brewery', 'Cocktail Bar',  'Coffee Shop', 'Lounge', 'Gay Bar', 'Gastropub', 'Hookah Bar', 
+             'Rock Club', 'Wine Bar']
+class_art = ['Bookstore', 'Theater', 'Performing Arts Venue', 'Indie Movie Theater', 'Museum', 'Art Gallery', 'Library']
+class_fitness = ['Yoga Studio', 'Gym', 'Gym / Fitness Center']
+class_store = ['Department Store', 'Grocery Store', 'Market', 'Park', 'Plaza', 'Shoe Store', 'Shopping Mall', 'Wine Shop']
+
+
+# In[16]:
+
+
+def __condition__ (x):
+    for restaurant in class_restaurant:
+        if x in class_restaurant:
+            return 'Restaurant'
+        elif x in class_coffe:
+            return 'Coffe'
+        elif x in class_bar:
+            return 'Bar'
+        elif x in class_art:
+            return 'Art'
+        elif x in class_fitness:
+            return 'Fitness'
+        elif x in class_store:
+            return 'Store'
+        else:
+            return x
+
+
+# In[17]:
+
+
+venues_group['Class'] = venues_group.apply(lambda x: __condition__(x["Venue Category"]), axis=1)
+
+
+# In[18]:
+
+
+venues_group = venues_group[['Class', 'Venue']]
+venues_group = venues_group.groupby(by='Class').sum()
+
+
+# In[19]:
+
+
+venues_group.shape
+
+
+# #### Plotting the venues by categories
+
+# In[20]:
+
+
+w = venues_group.sort_values(by='Venue', ascending=False)
+
+w.plot(kind='bar', figsize=(12, 6), color=(0.2, 0.4, 0.6, 0.6))
+
+plt.xlabel('') # add to x-label to the plot
+plt.ylabel('Number of venues', fontsize=12) # add y-label to the plot
+plt.title('Venues by categories in {}.'.format(address), fontsize=15) # add title to the plot
+plt.xticks(ha='right', rotation=55, fontsize=16)
+plt.yticks(fontsize=12)
+
+plt.show()
+
+
+# #### Map by Category
+
+# In[46]:
+
+
+categories_map = nearby_venues
+categories_map.shape
+
+
+# In[47]:
+
+
+categories_map['Class'] = categories_map.apply(lambda x: __condition__(x["Venue Category"]), axis=1)
+categories_map.head()
+
+
+# In[51]:
+
+
+categories_map['Class'].unique()
+
+
+# In[52]:
+
+
+c1 = categories_map.loc[categories_map['Class']=='Restaurant']
+c2 = categories_map.loc[categories_map['Class']=='Bar']
+c3 = categories_map.loc[categories_map['Class']=='Coffe']
+c4 = categories_map.loc[categories_map['Class']=='Hotel']
+c5 = categories_map.loc[categories_map['Class']=='Art']
+c6 = categories_map.loc[categories_map['Class']=='Fitness']
+c7 = categories_map.loc[categories_map['Class']=='Store']
+
+
+# In[63]:
+
+
+c_map = folium.Map(location=[lat, lng], zoom_start=13)
+
+folium.features.CircleMarker(
+    [lat, lng],
+    radius=10,
+    color='red',
+    popup=address,
+    fill = True,
+    fill_color = 'red',
+    fill_opacity = 0.6
+).add_to(c_map)
+
+for lat, lng, label in zip(c1['Venue Latitude'], c1['Venue Longitude'], c1['Class']):
+    folium.features.CircleMarker(
+        [lat, lng],
+        radius=5,
+        color='blue',
+        popup=label,
+        fill = True,
+        fill_color='blue',
+        fill_opacity=0.6
+    ).add_to(c_map)
+    
+for lat, lng, label in zip(c2['Venue Latitude'], c2['Venue Longitude'], c2['Class']):
+    folium.features.CircleMarker(
+        [lat, lng],
+        radius=5,
+        color='green',
+        popup=label,
+        fill = True,
+        fill_color='green',
+        fill_opacity=0.6
+    ).add_to(c_map)
+    
+    
+for lat, lng, label in zip(c3['Venue Latitude'], c3['Venue Longitude'], c3['Class']):
+    folium.features.CircleMarker(
+        [lat, lng],
+        radius=5,
+        color='orange',
+        popup=label,
+        fill = True,
+        fill_color='orange',
+        fill_opacity=0.6
+    ).add_to(c_map)    
+
+    
+text = 'This is the end of this project for now'
+
+iframe = folium.IFrame(text, width=700, height=450)
+popup = folium.Popup(iframe, max_width=3000)
+
+Text = folium.Marker(location=[lat,lng], popup=popup,
+                     icon=folium.Icon(icon_color='green'))
+c_map.add_child(Text)    
+    
+    
+c_map
+
+
+# In[41]:
+
+
+
 
 
 # In[ ]:
